0e61ce85e6b6633458adad3339b4cea21ad4e942
andbeck <andrew.beckerman@gmail.com>
2018-02-19 10:24:37 +0000

    removed APS273 old and wrong stuff

--- APS273/TemplateForQ3-273.R
+++
@@ -1,17 +0,0 @@
-# ANNOTATE HERE
-# (NAME/Student Number)
-# (Date?)
-# Anything else
-# Question 3a/3b
-
-# STEP 1: projection matrix from the word doc
-projMat<-matrix(
-	c(0, 0, 0, 4.665, 61.896,	0.675, 0.703, 0, 0, 0,	0, 0.047, 0.657,0,0,
-	0, 0, 0.019, 0.682, 0, 
-	0, 0, 0, 0.061, 0.8091), 
-	nrow = 5, byrow = TRUE)
-
-# make sure it looks like what is in the word doc.
-projMat
-
-# STEP 2:
